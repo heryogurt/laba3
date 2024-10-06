833ecd5dfe5fe241860fb77a237fb6c832d54420
heryogurt <92820593+heryogurt@users.noreply.github.com>
2024-10-07 01:14:19 +0300

    main.go

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	var ch string
 	fmt.Println("enter number: ")
 	fmt.Scan(&ch)
-	for i := 0; i < len(ch); i++ {
-		a := ch[i] - 48
-		fmt.Print(a * a)
+	for _, a := range ch {
+		y, e := strconv.Atoi(string(a))
+		if e == nil {
+			fmt.Print(y * y)
+		}
 	}
 }
